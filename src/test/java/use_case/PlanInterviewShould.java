04377e8a0ad2ada6478a24439e1fd4d35dd9158c
Sepehr NAMDAR <sepehr_gsfc@yahoo.com>
2020-11-27 22:50:57 +0100

    Solution-1/Approval Tests

--- src/test/java/use_case/PlanInterviewShould.java
+++ src/test/java/use_case/PlanInterviewShould.java
@@ -1,7 +1,6 @@
 package use_case;
 
 import model.Interview;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -12,8 +11,37 @@ import static org.approvaltests.combinations.CombinationApprovals.verifyAllCombi
 
 public class PlanInterviewShould {
 
+    private CandidateRepository candidates;
+    private RecruiterRepository recruiters;
+    private InterviewRepository interviews;
+
+    @BeforeEach
+    public void init() {
+        candidates = new FakeCandidates();
+        recruiters = new FakeRecruiters();
+        interviews = new FakeInterviews();
+    }
+
     @Test
-    void works() {
-        Assertions.assertThat(true).isTrue();
+    void find_first_available_recruiter_who_can_test_candidate() {
+        verifyAllCombinations(
+                this::planInterview,
+                new String[] {"123", "456", "789", null, "", "XYZ", "456-por"},
+                new LocalDate[] {
+                        of(2021, 2, 20),
+                        of(2021, 2, 21),
+                        of(2021, 2, 22)});
+    }
+
+    private Interview planInterview(String candidateId, LocalDate availability) {
+        PlanInterview planner = new PlanInterview(candidates, recruiters, interviews);
+        return planner.plan(candidateId, availability);
+    }
+
+    private static class FakeInterviews implements InterviewRepository {
+        @Override
+        public void save(Interview interview) {
+
+        }
     }
 }
